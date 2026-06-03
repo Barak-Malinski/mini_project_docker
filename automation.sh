94238cdb5d89dc64cc12a47b3bc7700b1956a36f
Barak-Malinski <barak94@gmail.com>
2026-06-03 15:06:40 +0300

    updated automation.sh file and added new server ansible-slave-2-db-1 for now

--- automation.sh
+++ automation.sh
@@ -1,14 +1,26 @@
 #!/bin/bash
 
+pwd
 cd ansible-master
 docker build -t ansible-master:v0.1 . 
 cd ..
 pwd
-cd "ansible-slave-1"
+cd ansible-slave-1
 docker build -t ansible-app-slave:v0.1 . 
 cd ..
 pwd
+cd ansible
 docker compose up -d
+cd ..
 pwd
-cd "ansible-slave-1"
+cd ansible-slave-1
 docker cp ansible-slave:/home/ansible/.ssh/id_rsa .
+docker cp id_rsa ansible-master:/app/id_rsa
+docker exec -it ansible-master bash -c "chmod 600 /app/id_rsa"
+docker exec -it ansible-master bash -c "ls -la /app/id_rsa"
+docker exec -it ansible-master bash -c "mkdir -p ~/.ssh"
+docker exec -it ansible-master bash -c "ssh-keyscan ansible-slave >> ~/.ssh/known_hosts"
+cd ..
+pwd
+cd ansible-slave-2
+docker compose up -d
